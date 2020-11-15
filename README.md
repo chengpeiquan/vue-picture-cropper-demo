0593d2c78428cdf258d819da68115d5726844166
chengpeiquan <chengpeiquan@chengpeiquan.com>
2020-11-16 00:51:58 +0800

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+# vue-image-cropper
+
+一个基于cropper.js，支持Vue 3.0的图片裁切工具
+
+## 功能
+
+可以在Vue 3.0里使用的图片裁切工具。
+
+
--- src/vue-image-cropper.tsx
+++ src/vue-image-cropper.tsx
@@ -9,14 +9,6 @@ import {
 import Cropper from 'cropperjs'
 import 'cropperjs/dist/cropper.css'
 
-
-
-/** 
- * 暴露一个实例，用于在父级组件操作实例的方法
- */
-export const cropper = ref<any>(null);
-
-
 /** 
  * 定义组件
  */
@@ -35,15 +27,20 @@ const VueImageCropper = defineComponent({
     }
   },
   setup (props) {
+    const cropper = ref<any>(null);
     const cropperImg = ref<any>(null);
 
+    
     /** 
      * 初始化插件
      * 必须等待视图渲染完毕后再执行
      */
     const initCropper = (): void => {
       nextTick( () => {
+        // 初始化实例
         cropper.value = new Cropper(cropperImg.value, props.options);
+
+        // 暴露一个实例，用于在父级组件操作实例的方法
         VueImageCropper.cropper = cropper.value;
       });
     };
@@ -62,11 +59,7 @@ const VueImageCropper = defineComponent({
      * 执行初始化
      */
     onMounted( () => {
-      // 初始化
       initCropper();
-
-      // 给实例绑定一个方法
-      // VueImageCropper.cropper = cropper;
     });
 
 
